5709c8234cb0c2c9c2342d0c845be0cfef0c4332
Eweh23 <92167640+Eweh23@users.noreply.github.com>
2021-10-15 15:30:08 +0200

    Add files via upload

--- Skol/Program.cs
+++ Skol/Program.cs
@@ -6,7 +6,22 @@ namespace Skol
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random gen = new Random();
+            int slump = gen.Next(100);
+            int gissningar=0;
+            int gissning=101;
+            Console.WriteLine("Gissa ett tal mellan 1 och 100");
+            while(gissning != slump){
+                gissning=int.Parse(Console.ReadLine());
+                gissningar++;
+                if(slump<gissning){
+                Console.WriteLine("Talet är lägre");
+                }
+                else if(slump>gissning){
+                Console.WriteLine("Talet är högre");
+                }
+            }
+            Console.WriteLine("Bra jobbat du gissade rätt på "+gissningar+" försök");
         }
     }
 }
